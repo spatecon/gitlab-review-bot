a8cdab22923cca5c8d1448e084aa7fe85d6d5179
Ilya Pavlyukov <ilya.pavlyukov@tabby.ai>
2022-11-23 15:32:50 +0300

    reviewers

--- internal/pkg/client/gitlab/reviewers.go
+++ internal/pkg/client/gitlab/reviewers.go
@@ -9,14 +9,20 @@ import (
 )
 
 func (c *Client) SetReviewers(mr *ds.MergeRequest, reviewers []int) error {
-	log.Info().Int("project_id", mr.ProjectID).Int("mr_id", mr.IID).Ints("reviewers", reviewers).Msg("reviewers set")
+	l := log.With().Int("project_id", mr.ProjectID).Int("mr_id", mr.IID).Logger()
+
+	l.Info().Ints("reviewers", reviewers).Msg("reviewers set")
 
 	c.rl.Take()
-	actual, _, err := c.gitlab.MergeRequests.UpdateMergeRequest(mr.ProjectID, mr.IID, &gitlab.UpdateMergeRequestOptions{
+	actual, resp, err := c.gitlab.MergeRequests.UpdateMergeRequest(mr.ProjectID, mr.IID, &gitlab.UpdateMergeRequestOptions{
 		ReviewerIDs: &reviewers,
 	})
 	if err != nil {
 		return errors.Wrap(err, "error calling gitlab apid to set reviewers")
+	}
+
+	if resp != nil {
+		l.Info().Ints("reviewers", reviewers).Str("status", resp.Status).Msg("reviewers set")
 	}
 
 	needed := make(map[int]bool, len(reviewers))
