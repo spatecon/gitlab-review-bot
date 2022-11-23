b594c49504fc5996da02a870e429f8ef3ed0cfc3
Ilya Pavlyukov <ilya.pavlyukov@tabby.ai>
2022-11-23 14:18:51 +0300

    set reviewers and MR state && dockerignore

--- internal/pkg/client/gitlab/reviewers.go
+++ internal/pkg/client/gitlab/reviewers.go
@@ -11,12 +11,23 @@ import (
 func (c *Client) SetReviewers(mr *ds.MergeRequest, reviewers []int) error {
 	log.Info().Int("project_id", mr.ProjectID).Int("mr_id", mr.IID).Ints("reviewers", reviewers).Msg("reviewers set")
 
-	_, _, err := c.gitlab.MergeRequests.UpdateMergeRequest(mr.ProjectID, mr.IID, &gitlab.UpdateMergeRequestOptions{
-		Description: gitlab.String(mr.Description),
+	actual, _, err := c.gitlab.MergeRequests.UpdateMergeRequest(mr.ProjectID, mr.IID, &gitlab.UpdateMergeRequestOptions{
 		ReviewerIDs: &reviewers,
 	})
 	if err != nil {
 		return errors.Wrap(err, "error calling gitlab apid to set reviewers")
+	}
+
+	needed := make(map[int]bool, len(reviewers))
+
+	for _, basicUser := range actual.Reviewers {
+		needed[basicUser.ID] = true
+	}
+
+	for _, gitlabID := range reviewers {
+		if _, ok := needed[gitlabID]; !ok {
+			return errors.Errorf("reviewer not set: %d", gitlabID)
+		}
 	}
 
 	return nil
